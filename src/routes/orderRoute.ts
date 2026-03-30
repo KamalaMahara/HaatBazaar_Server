90cfa3454385101ef5f7ec38daa9694540a8582f
Kamala Mahara <kmlamahara@gmail.com>
2026-03-30 23:02:40 +0545

    fix: change cancel order route method from POST to PATCH

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -14,7 +14,7 @@ router.route("/admin/change-order-status/:id").post(userMiddleware.isUserLoggedI
 router.route("/admin/delete-order/:id").post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Roles.Admin), errorHandler(orderController.deleteOrder))
 
 
-router.route("/cancel-order/:id").post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Roles.Customer), errorHandler(orderController.cancelMyOrder))
+router.route("/cancel-order/:id").patch(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Roles.Customer), errorHandler(orderController.cancelMyOrder))
 
 router.route("/:id").get(userMiddleware.isUserLoggedIn, errorHandler(orderController.fetchMyOrderDetail))
 
